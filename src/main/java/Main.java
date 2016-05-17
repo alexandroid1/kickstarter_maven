38e9e0f91d004b0642a36895389d855996f14889
Oleksandr <avpavlov96@gmail.com>
2016-05-17 13:33:46 +0300

    some files deleted , Spring pom added

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ua.com.goit.gojava.kickstarter.Category;
+import ua.com.goit.gojava.kickstarter.dao.CategoriesDAO;
+
+import java.util.List;
+
+/**
+ * Created by ALEX on 17.05.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("/application-context.xml");
+        CategoriesDAO categoriesDAO = context.getBean(CategoriesDAO.class);
+        List<Category> categories = categoriesDAO.getCategories();
+        System.out.println(categories.toString());
+    }
+}
